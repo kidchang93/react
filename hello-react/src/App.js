dba188ad7d555a9a6edb01a0c9988a921147d4c9
kidchang93 <lck930721@gmail.com>
2024-05-20 13:45:11 +0900

    react css 적용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -5,15 +5,22 @@ function App() {
   const name = "리액트";
   const name1 = undefined;
   const style = {
-    // backgroud-color 는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성됩니다.
+    // background-color 는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성됩니다.
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: "48px", // font-size -> fontSize
+    fontWeight: "bold", // font-weight -> fontWeight
+    padding: 16, // 단위를 생략하면 px로 지정됩니다.
   };
   return (
-    <div>
+    <div style={style}>
       {/* {name == "리액트" ? <h1>리액트 입니다.</h1> : <h2>리액트가 아닙니다.</h2>} */}
       {/* {name === "리액트" ? <h1>리액트입니다.</h1> : null} */}
       {/* {name === "리액트" && <h1>리액트입니다.</h1>} */}
-      name || '값이 undefined' 입니다. ;<br></br>
-      {name1 || "리액트"};
+      {/* name || '값이 undefined' 입니다. ;<br></br> */}
+      {/* {name1 || "리액트"}; */}
+      {name}
+      <div className="react">{name}</div>
     </div>
   );
 }
