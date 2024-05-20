4c40a336e47f0f4eedb34341ec0c92759a31c9ef
kidchang93 <lck930721@gmail.com>
2024-05-20 14:36:05 +0900

    비구조화할당

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,31 +1,13 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import MyComponent from "./MyComponent";
 
-function App() {
-  const name = "리액트";
-  const name1 = undefined;
-  const style = {
-    // background-color 는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성됩니다.
-    backgroundColor: "black",
-    color: "aqua",
-    fontSize: "48px", // font-size -> fontSize
-    fontWeight: "bold", // font-weight -> fontWeight
-    padding: 16, // 단위를 생략하면 px로 지정됩니다.
-  };
+const App = () => {
   return (
-    <div style={style}>
-      {/* {name == "리액트" ? <h1>리액트 입니다.</h1> : <h2>리액트가 아닙니다.</h2>} */}
-      {/* {name === "리액트" ? <h1>리액트입니다.</h1> : null} */}
-      {/* {name === "리액트" && <h1>리액트입니다.</h1>} */}
-      {/* name || '값이 undefined' 입니다. ;<br></br> */}
-      {/* {name1 || "리액트"}; */}
-      {name}
-      <div className="react">{name}</div>
-      <input />
-      <MyComponent />
+    <div>
+      <MyComponent>리액트</MyComponent>
     </div>
   );
-}
+};
 
 export default App;
